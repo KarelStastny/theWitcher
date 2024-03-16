b40694c19de0ad3e352043b7f3940e9e747d82ba
Karel Šťastný <karell.stastny29@gmail.com>
2024-03-16 17:09:18 +0100

    training/reducer - changing data

---
+++ src/data/armors.js
@@ -0,0 +1,80 @@
+export const armors = [
+    {
+        name: "griffinArmors",
+        levels: [
+            {
+                id: "griffin-1",
+                level: 1,
+                armory: 50,
+                darkColor: true,
+                endurance: 800,
+            },
+            {
+                id: "griffin-2",
+                level: 2,
+                armory: 60,
+                darkColor: false,
+                endurance: 900,
+            },
+            {
+                id: "griffin-3",
+                level: 3,
+                armory: 70,
+                darkColor: true,
+                endurance: 1000,
+            }
+        ]
+    },
+    {
+        name: "bearArmors",
+        levels: [
+            {
+                id: "bear-1",
+                level: 1,
+                armory: 55,
+                darkColor: true,
+                endurance: 850,
+            },
+            {
+                id: "bear-2",
+                level: 2,
+                armory: 65,
+                darkColor: false,
+                endurance: 950,
+            },
+            {
+                id: "bear-3",
+                level: 3,
+                armory: 75,
+                darkColor: true,
+                endurance: 1050,
+            }
+        ]
+    },
+    {
+        name: "wolfArmors",
+        levels: [
+            {
+                id: "wolf-1",
+                level: 1,
+                armory: 52,
+                darkColor: false,
+                endurance: 820,
+            },
+            {
+                id: "wolf-2",
+                level: 2,
+                armory: 62,
+                darkColor: true,
+                endurance: 920,
+            },
+            {
+                id: "wolf-3",
+                level: 3,
+                armory: 72,
+                darkColor: false,
+                endurance: 1020,
+            }
+        ]
+    }
+];
---
+++ src/data/weapon.js
@@ -0,0 +1,50 @@
+export const mainWeapon = {
+    mage: [
+        iceMagic,
+        fireMagic,
+        dark
+    ],
+    witcher: [
+        sword,
+        silverSword,
+
+    ]
+}
+
+export const witcherMagic = {
+    Aard: {
+        level_1: "easy",
+        level_2: "middle",
+        level_3: "hard"
+    },
+    Igni: {
+        level_1: "easy",
+        level_2: "middle",
+        level_3: "hard"
+    },
+    Yrden: {
+        level_1: "easy",
+        level_2: "middle",
+        level_3: "hard"
+    },
+    Quen: {
+        level_1: "easy",
+        level_2: "middle",
+        level_3: "hard"
+    },
+    Axii: {
+        level_1: "easy",
+        level_2: "middle",
+        level_3: "hard"
+    }
+};
+
+export const petard = {
+    sm: "Samum",
+    dg: "DevilsPuffball",
+    dp: "DancingStar",
+    mb: "MoonDust",
+    gr: "Grapeshot",
+    dr: "DragonDream",
+    nb: "NorthernWind"
+};
